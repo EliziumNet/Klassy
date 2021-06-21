77675fc09b363cc6b7f3810fca211bd11b818649
plastikfan <plastikfan@outlook.com>
2021-06-21 11:39:33 +0100

    Remove rogue override in New-ProxyGit

--- Elizium.Klassy/Public/New-ProxyGit.ps1
+++ Elizium.Klassy/Public/New-ProxyGit.ps1
@@ -54,28 +54,26 @@ function New-ProxyGit {
   [OutputType([ProxyGit])]
   param(
     [Parameter()]
-    [hashtable]$Overrides
+    [hashtable]$Overrides = @{}
   )
-  [hashtable]$script:overrides = @{
-    'ReadLogRange' = [scriptblock] {
-      return $_headTagData.TimeStamp;
-    }
-  }
-
   [ProxyGit]$proxy = [ProxyGit]::new();
-  [array]$members = ($proxy | Get-Member -MemberType Property).Name;
 
-  $Overrides.PSBase.Keys | ForEach-Object {
-    [string]$name = $_;
+  if ($Overrides.Count -gt 0) {
+    [array]$members = ($proxy | Get-Member -MemberType Property).Name;
 
-    if ($members -contains $name) {
-      $proxy.$name = $Overrides[$name];
+    $Overrides.PSBase.Keys | ForEach-Object {
+      [string]$name = $_;
+
+      if ($members -contains $name) {
+        $proxy.$name = $Overrides[$name];
+      }
+      else {
+        throw [System.Management.Automation.MethodInvocationException]::new(
+          "'$name' does not exist on Proxy"
+        );
+      }
     }
-    else {
-      throw [System.Management.Automation.MethodInvocationException]::new(
-        "'$name' does not exist on Proxy"
-      );
-    }
+
   }
 
   return $proxy;
