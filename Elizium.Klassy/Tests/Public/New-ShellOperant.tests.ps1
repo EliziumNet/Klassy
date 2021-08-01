95104bdc2293d4a81aee095330cf0f00da0693e7
plastikfan <plastikfan@outlook.com>
2021-08-01 19:51:25 +0100

    feat(shelly): change by adding date time format param to New-ShellOperant (#20)

--- Elizium.Klassy/Tests/Public/New-ShellOperant.tests.ps1
+++ Elizium.Klassy/Tests/Public/New-ShellOperant.tests.ps1
@@ -1,39 +1,69 @@
 Import-Module .\Output\Elizium.Klassy\Elizium.Klassy.psm1
 Describe 'New-ShellOperant' {
-  BeforeEach {
-    InModuleScope Elizium.Klassy {
-      Mock -ModuleName Elizium.Klassy get-CurrentTime {
-        return '04-Jan-2021';
+  Context 'given: default date format' {
+    BeforeEach {
+      InModuleScope Elizium.Klassy {
+        Mock -ModuleName Elizium.Klassy get-CurrentTime {
+          return '2021-01-04_10-28-34';
+        }
       }
     }
-  }
 
-  Context 'given: invoked with defaults' {
-    It 'should: return UndoRename instance' {
-      InModuleScope Elizium.Klassy {
-        [UndoRename]$operant = New-ShellOperant -BaseFilename 'undo-rename' `
-          -Directory $TestDrive;
+    Context 'given: invoked with defaults' {
+      It 'should: return UndoRename instance' {
+        InModuleScope Elizium.Klassy {
+          [UndoRename]$operant = New-ShellOperant -BaseFilename 'undo-rename' `
+            -Directory $TestDrive;
 
-        $operant | Should -Not -BeNullOrEmpty;
-        $operant.GetType() | Should -Be UndoRename;
+          $operant | Should -Not -BeNullOrEmpty;
+          $operant.GetType() | Should -Be UndoRename;
 
-        [string]$fullPath = $operant.Shell.FullPath;
-        $fullPath | Should -Match '04-Jan-2021';
+          [string]$fullPath = $operant.Shell.FullPath;
+          $fullPath | Should -Match '2021-01-04';
+        }
       }
     }
-  }
 
-  Context 'given: invoked explicitly with UndoRename and PoShShell' {
-    It 'should: return UndoRename instance' {
+    Context 'given: invoked explicitly with UndoRename and PoShShell' {
+      It 'should: return UndoRename instance' {
+        InModuleScope Elizium.Klassy {
+          [UndoRename]$operant = New-ShellOperant -BaseFilename 'undo-rename' `
+            -Directory $TestDrive -Operant 'UndoRename' -Shell 'PoShShell';
+
+          $operant | Should -Not -BeNullOrEmpty;
+          $operant.GetType() | Should -Be UndoRename;
+
+          [string]$fullPath = $operant.Shell.FullPath;
+          $fullPath | Should -Match '2021-01-04';
+        }
+      }
+    }
+  } # default date format
+
+  Context 'given: custom date format' {
+    BeforeEach {
       InModuleScope Elizium.Klassy {
-        [UndoRename]$operant = New-ShellOperant -BaseFilename 'undo-rename' `
-          -Directory $TestDrive -Operant 'UndoRename' -Shell 'PoShShell';
+        Mock -ModuleName Elizium.Klassy get-CurrentTime {
+          param(
+            [string]$Format
+          )
+          return [DateTime]::new(2021, 1, 4, 10, 28, 34).ToString($Format);
+        }
+      }
+    }
 
-        $operant | Should -Not -BeNullOrEmpty;
-        $operant.GetType() | Should -Be UndoRename;
+    Context 'given: invoked with custom date format' {
+      It 'should: return UndoRename instance with customised date' {
+        InModuleScope Elizium.Klassy {
+          [UndoRename]$operant = New-ShellOperant -BaseFilename 'undo-rename' `
+            -Directory $TestDrive -DateTimeFormat 'dd-MMM-yyyy_HH-mm-ss';
 
-        [string]$fullPath = $operant.Shell.FullPath;
-        $fullPath | Should -Match '04-Jan-2021';
+          $operant | Should -Not -BeNullOrEmpty;
+          $operant.GetType() | Should -Be UndoRename;
+
+          [string]$fullPath = $operant.Shell.FullPath;
+          $fullPath | Should -Match '04-jan-2021';
+        }
       }
     }
   }
